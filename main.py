15d8146bf90799338b54f67bf9e8284bf926e2a4
Ezra Mirek <113930317+emirek2@users.noreply.github.com>
2022-10-27 17:22:22 +0000

    functions

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from methods_help import method_help
-# from functions import function
+# from methods_help import method_help
+from functions import function
 # from returnStuff import returnS
+from functions import welcome
+from functions import add_numbers
+from functions import num_dividedFrom_add_numbers
 
-method_help()
-# function()
+
+# method_help()
+function()
 # returnS()
+
+
+wel=welcome("Ezra")
+print(wel)
+
+
+added=add_numbers(1,3)
+print(added)
+
+divide=num_dividedFrom_add_numbers(45,added)
+print(divide)
