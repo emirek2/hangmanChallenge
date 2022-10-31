91a685bbe896a5dc3fa55c2ff903cc839cc8e08c
Ezra Mirek <113930317+emirek2@users.noreply.github.com>
2022-10-31 16:48:49 +0000

    dynamic functions

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 # # from methods_help import method_help
 # from functions import function
-from returnStuff import power, usd_to_eur, reverse_word
+# from returnStuff import power, usd_to_eur, reverse_word
 # from functions import welcome
 # # from functions import add_numbers
 # # from functions import num_dividedFrom_add_numbers
 # from functions import prob1
 # from functions import square
+from dynamic_functions import check_3Digits
 
 
+result=check_3Digits(7)
+print(result)
 # method_help()
 # function()
 # returnS()
-powers=power(3,7)
-print(powers)
-eur=usd_to_eur(1600)
-print(eur)
-reverse_word("python")
+# powers=power(3,7)
+# print(powers)
+# eur=usd_to_eur(1600)
+# print(eur)
+# reverse_word("python")
 
 
 # wel=welcome("Ezra")
